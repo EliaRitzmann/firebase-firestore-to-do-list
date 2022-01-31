1fcff3f6e3b6fca28d62f37ef3efd304db9e5ea1
EliaRitzmann <E.Ritzmann.inf20@stud.bbbaden.ch>
2022-01-31 23:27:07 +0100

    firestore

--- src/components/Element.js
+++ src/components/Element.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-export const Element = () => {
-  return <div>
-      <h1 className='text-green-700 bg-red-300 '>Test</h1>
+export const Element = (props) => {
+  return <div className='flex '>
+      <h1 className='text-green-700 '>{props.item.text}</h1>
+      <h2></h2>
+      <button>save</button>
+      <button>delete</button>
   </div>;
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,72 @@
 import { isValidTimestamp } from '@firebase/util';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { LoginPopup } from '../components/LoginPopup';
 import { useAuth } from '../contexts/FirebaseContext';
+import pic from "../images/blank-profile-picture.png"
+import { Element } from '../components/Element'
+import { collection, getDocs } from 'firebase/firestore';
+import { firestore } from '../api/firebase';
 
 export const Home = () => {
-  const {user, logOut} = useAuth();
+  const { user, logOut } = useAuth();
+  const [list, setlist] = useState([]);
+  const listCollectionRef = collection(firestore, "to-do-list")
 
-  function handleLogOut(){
-    try{
+
+  useEffect(() => {
+
+    async function getList(){
+      const data = await getDocs(listCollectionRef)
+      setlist(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+
+    getList()
+  }, [])
+
+  function handleLogOut() {
+    try {
       logOut()
-  }catch(error){
+    } catch (error) {
       console.log(error)
-  }
+    }
   }
 
-  return <div className=''>
-      <h1>Main</h1>
-      <h1>{user?.email}</h1>
-      <button onClick={handleLogOut}>Log Out</button>
-      <LoginPopup trigger={user == null}/>
-  </div>;
+  const element = []
+
+  for (var i = 0; i < list.length; i++) {
+    element.push(<Element item={list[i]}></Element>)
+  }
+
+  return (
+    <div>
+      <LoginPopup trigger={user == null} />
+      <div>
+        <nav className='flex items-start justify-between m-3 h-10 border-b-2 '>
+          <div className='w-40'>
+            <h1 class="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+            >
+              To do List
+            </h1>
+          </div>
+
+          <div className='flex gap-2 items-center justify-end w-40'>
+            <button className='text-lg font-medium' onClick={handleLogOut}>{user?.displayName ? user.displayName : user?.email}</button>
+            <img class="w-8 h-8 rounded-full" src={user?.photoURL ? user.photoURL : pic} alt="user photo" />
+          </div>
+        </nav>
+
+        <main className='flex items-center flex-col'>
+          <div className='divide-y divide-slate-700 '>
+            {element}
+          </div>
+          <button className='bg-red-400 text-white py-2 px-4 rounded inline-flex items-center mt-2' onClick={handleLogOut}>
+          Log out
+        </button>
+      </main>
+    </div>
+
+
+    </div >)
+
 };
