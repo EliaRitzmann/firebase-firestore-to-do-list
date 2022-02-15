e12ecba669dd2600e1aad646f22c499a79f870f4
EliaRitzmann <E.Ritzmann.inf20@stud.bbbaden.ch>
2022-02-15 15:04:13 +0100

    router fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ import { useAuth } from '../contexts/FirebaseContext';
 //Firestore
 import {
   collection, query, where,
-  doc, addDoc, onSnapshot, orderBy
+  onSnapshot
 } from 'firebase/firestore';
 import { firestore } from '../api/firebase';
 
@@ -13,10 +13,11 @@ import { firestore } from '../api/firebase';
 import { AddCategory } from './AddCategory';
 import { CategoryElement } from './CategoryElement';
 import { getIcons } from '../icons/Icons';
+import { useNavigate } from 'react-router-dom';
 
 
 export const Sidebar = () => {
-
+  const navigate = useNavigate();
   const { user } = useAuth()
 
   const [category, setCategory] = useState([]);
@@ -40,30 +41,42 @@ export const Sidebar = () => {
     ),
     []);
 
+    function btn_home(){
+      navigate("/")
+    }
+
+    function btn_fav(){
+      navigate("/favourites")
+    }
+
+    function btn_all(){
+      navigate("/all")
+    }
+
   return <div className='flex flex-col items-center gap-2'>
-    <a href="/" className='inline-flex w-4/5 h-10 items-center justify-between rounded-lg px-2 bg-white shadow shadow-gray-400'>
+    <button onClick={btn_home} className='inline-flex w-4/5 h-10 items-center justify-between rounded-lg px-2 bg-white shadow shadow-gray-400'>
       <div className='flex gap-2'>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
         </svg>
         <span className='font-semibold'>Home</span>
       </div>
-    </a>
+    </button>
 
-    <a href="/all" className='inline-flex w-4/5 h-10 items-center justify-between rounded-lg px-2 bg-white shadow shadow-gray-400'>
+    <button onClick={btn_all} className='inline-flex w-4/5 h-10 items-center justify-between rounded-lg px-2 bg-white shadow shadow-gray-400'>
       <div className='flex gap-2'>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
         </svg>
         <span className='font-semibold'>All</span>
       </div>
-    </a>
-    <a href="/favourites" className='inline-flex w-4/5 h-10 items-center justify-between rounded-lg px-2 bg-white shadow shadow-gray-400'>
+    </button>
+    <button onClick={btn_fav} className='inline-flex w-4/5 h-10 items-center justify-between rounded-lg px-2 bg-white shadow shadow-gray-400'>
       <div className='flex gap-2'>
       {getIcons("heart", "black", "small")}
         <span className='font-semibold'>Favourites</span>
       </div>
-    </a>
+    </button>
     <h1 className='font-semibold '>Categories</h1>
     {elements}
     <AddCategory></AddCategory>
