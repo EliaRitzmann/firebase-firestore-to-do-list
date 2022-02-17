e5139fb0a1cb1da43344d6aa77885f735b786b88
EliaRitzmann <E.Ritzmann.inf20@stud.bbbaden.ch>
2022-02-17 22:09:59 +0100

    delete category

--- src/components/CategoryElement.js
+++ src/components/CategoryElement.js
@@ -6,7 +6,6 @@ import { firestore } from '../api/firebase';
 
 export const CategoryElement = (props) => {
   const navigate = useNavigate();
-  const [isExtended, setIsExtended] = useState(false)
   const [edit, setEdit] = useState(false)
   const [showModal, setShowModal] = useState(false);
   const [name, setName] = useState(props.item.name);
@@ -22,10 +21,6 @@ export const CategoryElement = (props) => {
 
   function toggleedit() {
     setEdit(prevEdit => !prevEdit)
-  }
-
-  function extend(){
-    setIsExtended(prevIsExtended => ! prevIsExtended)
   }
 
 
@@ -95,29 +90,13 @@ export const CategoryElement = (props) => {
         {getIcons(icon, color, "small")}
         <span className='font-semibold'>{name}</span>
       </div>
-      {isExtended ? 
-      <div className='flex items-center gap-0.5'>
-        <button className='bg-gray-300 rounded-md p-0.5' onClick={deleteCategory} >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-</svg>
-      </button>
       <button className='bg-gray-300 rounded-md p-0.5' onClick={toggleedit}>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-</svg>
-      </button>
-        </div>
-      
-      :
-      
-      <button className='bg-gray-300 rounded-md p-0.5' onClick={extend}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
         </svg>
       </button>
       
-      }
+      
     </div>
   }
 };
